90744486f9330ccf0e09a9551cc526a02cb7c8d1
Dev-Waqas232 <devwaqas232@gmail.com>
2025-11-25 17:18:16 +0500

    feat: added endpoint to return random quotes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI
-from typing import Optional
+from fastapi import FastAPI, HTTPException, status
+import requests
 
 from config import settings
 
@@ -9,6 +9,27 @@ app = FastAPI()
 @app.get("/")
 async def home():
     return {"message": f"Server is running on {settings.settings.PORT}"}
+
+
+@app.get("/quote")
+async def get_quote():
+    api_url = settings.settings.QUOTE_URL
+    if not api_url:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="API url for quotes API is not found",)
+
+    try:
+        response = requests.get(settings.settings.QUOTE_URL)
+        # Raise an exception for bad status if API returned a bad response
+        response.raise_for_status()
+
+        data = response.json()
+
+        return {"ok": True, "data": data}
+
+    except requests.exceptions.RequestException as e:
+        return {"error": f"Failed to fetch data {e}"}
+
 
 if __name__ == "__main__":
     import uvicorn
